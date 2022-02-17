e55421149fdea4b0eb78f46cecfcb001df497868
Ezequiel <ezecavallo@gmail.com>
2022-02-17 11:10:55 -0300

    Webpack prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -13,27 +12,27 @@ module.exports = {
     publicPath: "/",
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js"],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     {
+      //       loader: "html-loader",
+      //     },
+      //   ],
+      // },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(s*)css$/,
+        test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
@@ -59,27 +58,20 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "assets/[name].css",
+      filename: "assets/style/[name].css",
     }),
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"),
+          from: path.resolve(__dirname, "src", "assets", "images"),
           to: "assets/images",
         },
         {
-          from: path.resolve(__dirname, "src", "assets/music"),
-          to: "assets/music",
+          from: path.resolve(__dirname, "src", "assets", "favicon"),
+          to: "assets/images",
         },
       ],
     }),
-    new Dotenv(),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-    historyApiFallback: true,
-  },
 };
