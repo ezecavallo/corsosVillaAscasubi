cc31934fa61ad86bea68e9bac116f4e11a2d2247
Ezequiel <ezecavallo@gmail.com>
2023-02-10 20:28:09 -0300

    Update

--- src/success.js
+++
@@ -1,7 +0,0 @@
-import { getCookie } from "./utils";
-import "./assets/style/master.css";
-
-(function App() {
-  if (!getCookie("4ca5d171acaac2c5ca261c97b0d40383"))
-    return window.location.replace("/");
-})();
