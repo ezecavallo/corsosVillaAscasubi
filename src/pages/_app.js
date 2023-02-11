a5a17cf62307e13a873496ccf9126a8ccb9022e5
Ezequiel <ezecavallo@gmail.com>
2023-02-11 14:18:44 -0300

    Favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import { ChakraProvider } from "@chakra-ui/react";
 import Fonts from "../components/Fonts";
 import { ParallaxProvider } from "react-scroll-parallax";
@@ -19,6 +20,36 @@ export default function _App({ Component, pageProps }) {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/images/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/images/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta
+            name="msapplication-config"
+            content="/images/favicon/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <style jsx global>{`
           /* Other global styles such as 'html, body' etc... */
